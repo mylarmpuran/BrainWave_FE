640ae890e8cd73a9fb1437367465cc8a49a507f1
puran <mylarmpuran@gmail.com>
2025-01-19 14:16:58 +0530

    section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Button from "./components/Button"
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     </h1>
     <div className='pt-[4.75rem] lg:pt-[5.25rem overflow-hidden ' >
       <Header/>
+      <Hero/>
     </div>
     <div className=''><ButtonGradient/></div>
     
